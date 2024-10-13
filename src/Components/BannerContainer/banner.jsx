132e57bc836be64574c767e7d45bd3c9c02ff8c1
Sameer764 <sam2001raj@gmail.com>
2024-10-13 22:59:25 +0530

    desktop view complete

--- src/Components/BannerContainer/banner.jsx
+++ src/Components/BannerContainer/banner.jsx
@@ -9,38 +9,37 @@ function BannerContainer() {
 
 
     return (
-        <div className='Banner-container'>
-            <div className=' subcontainer'>
-                <div className='container1'>
-                    <div className='inner-container1'>
-                        <div className='mode-of-class'>
-                            <img className="star-image" src='/src/assets/img/starImg.svg' />
+        <div className='ppa-landing-page-tfp-banner-container'>
+                <div className='ppa-landing-page-tfp-container-right'>
+                    <div className='ppa-landing-page-tfp-inner-container-right'>
+                        <div className='ppa-landing-page-tfp-mode-of-class'>
+                            <img className="ppa-landing-page-tfp-star-image" src='/src/assets/img/starImg.svg' />
                             {/* <img src='/src/assets/img/Star 5.svg' /> */}
-                            <p className='mode-of-class-txt'>
+                            <p className='ppa-landing-page-tfp-mode-of-class-txt'>
                                 Offline Classroom
                             </p>
                         </div>
-                        <div className='inner-container1-logoTitle'>
-                            <div className='logo'>
-                                <img className="logo-img" src="/src/assets/img/LOGO.svg" />
+                        <div className='ppa-landing-page-tfp-inner-container-right-logoTitle'>
+                            <div className='ppa-landing-page-tfp-logo'>
+                                <img className="ppa-landing-page-tfp-logo-img" src="/src/assets/img/LOGO.svg" />
 
 
                             </div>
-                            <h2 className="logo-heading">Tech  Fellowship Program</h2>
+                            <h2 className="ppa-landing-page-tfp-logo-heading">Tech  Fellowship Program</h2>
                         </div>
 
                     </div>
-                    <div className='inner-container2'>
-                        <div className='row'>
+                    <div className='ppa-landing-page-tfp-inner-container-left'>
+                        <div className='ppa-landing-page-tfp-row'>
                             <img src='/src/assets/img/Frame 21073.svg' />
                             <img src='/src/assets/img/Frame 21074.svg' />
                         </div>
-                        <div className='row'>
+                        <div className='ppa-landing-page-tfp-row'>
                             <img src='/src/assets/img/Frame 21074 (1).svg' />
                             <img src='/src/assets/img/Frame 21076.svg' />
 
                         </div>
-                        <div className='row'>
+                        <div className='ppa-landing-page-tfp-row'>
                             <img src='/src/assets/img/Frame 21075.svg' />
                             <img src='/src/assets/img/Frame 21074 (2).svg' />
 
@@ -48,21 +47,21 @@ function BannerContainer() {
                     </div>
                 </div>
 
-            </div>
-            <div className='container2'>
-                <div className='title'>
-                    <h2>Tech Fellowship Program</h2>
-                    <p className='title-txt'>Launch Your Tech Career with Hands-On Experience in Real Projects from Leading Companies</p>
+
+            <div className='ppa-landing-page-tfp-container-left'>
+                <div className='ppa-landing-page-tfp-title'>
+                    <h2 className='ppa-landing-page-tfp-title-heading'>Tech Fellowship Program</h2>
+                    <p className='ppa-landing-page-tfp-title-txt'>Launch Your Tech Career with Hands-On Experience in Real Projects from Leading Companies</p>
                 </div>
-                <div className='features'>
+                <div className='ppa-landing-page-tfp-features'>
 
                     {data.map((item, index) => (
-                        <div className='list-of-features' style={{ width: item.size }}>
-                            <div className='list-box' key={index} >
-                                <p className='list-txt'>{item.f1}</p>
+                        <div className='ppa-landing-page-tfp-list-of-features'>
+                            <div className='ppa-landing-page-tfp-list-box' key={index} >
+                                <p className='ppa-landing-page-tfp-list-txt'>{item.f1}</p>
                             </div>
-                            <div className='list-box' key={index}>
-                                <p className='list-txt'>{item.f2}</p>
+                            <div className='ppa-landing-page-tfp-list-box' key={index}>
+                                <p className='ppa-landing-page-tfp-list-txt'>{item.f2}</p>
                             </div>
                         </div>
 
@@ -70,11 +69,11 @@ function BannerContainer() {
 
 
                 </div>
-                <div className='more-details'>
+                <div className='ppa-landing-page-tfp-more-details'>
                     <button onClick={handleClick} >
-                        <p className='button-txt'>Know more</p>
+                        <p className='ppa-landing-page-tfp-button-txt'>Know more</p>
                     </button>
-                    <p className='more-details-txt'> Live the Code, Don’t Just Learn It!</p>
+                    <p className='ppa-landing-page-tfp-more-details-txt'> Live the Code, Don't Just Learn It!</p>
                 </div>
             </div>
         </div>
